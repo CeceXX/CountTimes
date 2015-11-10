8728caf495257685d88946a400fe44cb375089dd
Cesare de Cal <hi@cesare.io>
2015-11-10 17:52:47 +0100

    The algorithm should now fully work

--- main.c
+++ main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
+void printElementsOfArray(char elements, int arrayLength) {
+}
+
 int countTimesElementIsFound(int elementToCount, char elements[], int arrayLength) {
     int timesElementWasFound = 0;
-    
-    for (int i = 0; i < elementToCount; i++) {
-        if (elementToCount == elements[i]) {
-            printf("%d was found %d time(s).", elementToCount);
-        }
-    }
-    return 1;
+    for (int i = 0; i < arrayLength; i++)
+        if (elementToCount == elements[i])
+            timesElementWasFound++;
+    return timesElementWasFound;
 }
 
 int main() {
-    char elements[] = {2, 2, 0, 0};
+    char numbers[] = {2, 2, 2, 0};
     int arrayLength = 4, elementToCount = 0;
-    
     while (elementToCount != arrayLength-1) {
-        countTimesElementIsFound(elementToCount, elements, arrayLength);
+        numbers[elementToCount] = countTimesElementIsFound(elementToCount, numbers, arrayLength);
+        elementToCount++;
     }
-    
+    printf(numbers, arrayLength);
     return 0;
 }
